dacb064b14c0aa22cb381fce0d66f7915864807a
Shahnawaz Khan <shahnawaz.khan@cwdin.com>
2026-02-26 15:14:02 +0530

    Refactor routing structure with tab navigation

---
+++ app/movies/[id].tsx
@@ -0,0 +1,15 @@
+import { Text, View } from 'react-native';
+import React from 'react';
+import { useLocalSearchParams } from 'expo-router'; 
+
+const MovieDetails = () => {
+     const { id } = useLocalSearchParams();
+    
+    return (
+        <View className=''>
+            <Text>MovieDetails: {id}</Text>
+        </View>
+    );
+};
+
+export default MovieDetails;
