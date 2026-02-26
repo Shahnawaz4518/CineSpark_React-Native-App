3bfa17d10affd792082f38021e60522cbe7c755c
Shahnawaz Khan <shahnawaz.khan@cwdin.com>
2026-02-26 11:51:47 +0530

    ReelRush custom UI, routing and config updates

---
+++ app/onboarding.tsx
@@ -0,0 +1,15 @@
+import { Text, View, StyleSheet } from 'react-native';
+import React from 'react'
+
+
+const Onboarding = () => {
+    return (
+        <View className="flex-1 justify-center items-center">
+            <Text className="text-3xl text-accent
+            font-bold">Onboarding</Text>
+        </View>
+    )
+}
+
+export default Onboarding
+const styles = StyleSheet.create({})    
